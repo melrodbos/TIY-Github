ca91200a4d6ffa3bf0cbbd2559d3f33286e0d607
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-06-10 23:58:30 -0400

    profile image and text replaced!

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,26 @@ $.getJSON('/api/github/users/octocat.json')
     .then(function(octocat){
         console.log(octocat);
 
-        $avatar = $('img#avatar');
+        $avatar = $('img', '.profile_avatar');
         $avatar.attr('src', octocat.avatar_url);
 
         $name = $('h1#name');
         $name.text(octocat.name);
 
-        
+        $login = $('h3#login');
+        $login.text(octocat.login);
 
+        $company = $('li#company');
+        $company.text(octocat.company);
 
+        $location = $('li#location');
+        $location.text(octocat.location);
 
+        $email = $('li#email');
+        $email.text(octocat.email);
+        // unable to replace url itself, but the placeholder was removed?
+        //Place holder was showing right next to the url...
+        $blog = $('li#blog');
+        $blog.text(octocat.blog);
 
   });
