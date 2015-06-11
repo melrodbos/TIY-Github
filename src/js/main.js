ce049e5f93149a5003a3506818dcc62cf82469c4
Melissa Rodríguez <m.rodriguez.bosques@gmail.com>
2015-06-10 23:22:08 -0400

    replacing text and attributes

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,15 @@ $.getJSON('/api/github/users/octocat.json')
     .then(function(octocat){
         console.log(octocat);
 
+        $avatar = $('img#avatar');
+        $avatar.attr('src', octocat.avatar_url);
+
+        $name = $('h1#name');
+        $name.text(octocat.name);
+
+        
+
+
+
+
   });
